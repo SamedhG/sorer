83b80f7c2d8d2d97143b6627830ef0f640b14e8e
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-02-01 13:05:01 -0500

    update comments and add sor.txt

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ fn main() {
     let reader = BufReader::new(f);
     let schema = infer_schema(reader);
 
+    // if argument is `-print_col_type`, we only need to infer the schema, in all other cases
+    // we parse the data into columnar format in memory.
     match parsed_args.option {
         Options::PrintColType(n) => {
             if n >= schema.len() as u64 {
@@ -37,6 +39,7 @@ fn main() {
     // number of threads to use
     let num_threads = 8;
 
+    // the total number of bytes to read
     let num_chars = if parsed_args.len == std::u64::MAX {
         std::fs::metadata(parsed_args.file.clone()).unwrap().len() - parsed_args.from
     } else {
@@ -47,22 +50,28 @@ fn main() {
     let step = (num_chars / 8) as u64;
 
     // setup the work array with the from / len for each thread
+    // each element in the work array is a tuple of (starting index, number of byte for this thread)
     let f: File = File::open(parsed_args.file.clone()).unwrap();
     let mut reader2 = BufReader::new(f);
-    let mut work: Vec<(u64, u64)> = Vec::with_capacity(8);
+    let mut work: Vec<(u64, u64)> = Vec::with_capacity(num_threads);
+
     work.push((parsed_args.from, step));
     let mut so_far = parsed_args.from;
-    let mut buff = Vec::new();
+    let mut buffer = Vec::new();
+
     for i in 1..num_threads {
         so_far += step;
+        // advance the reader to this threads starting index
         reader2.seek(SeekFrom::Start(so_far)).unwrap();
-        reader2.read_until(b'\n', &mut buff).unwrap();
+        reader2.read_until(b'\n', &mut buffer).unwrap();
         work.push((so_far, step));
-        work.get_mut(i - 1).unwrap().1 += buff.len() as u64 + 1;
-        buff.clear();
+
+        // TODO: explain methodology for this...
+        work.get_mut(i - 1).unwrap().1 += buffer.len() as u64 + 1;
+        buffer.clear();
     }
 
-    // initialize the threads
+    // initialize the threads with their own BufReader
     let mut threads = Vec::new();
     for w in work {
         let new_schema = schema.clone();
@@ -73,13 +82,13 @@ fn main() {
         }));
     }
 
-    // initialize the result vec
+    // initialize the resulting columnar data frame
     let mut parsed_data: Vec<Vec<Data>> = Vec::with_capacity(schema.len());
     for _ in 0..schema.len() {
         parsed_data.push(Vec::new());
     }
 
-    //combine the data
+    // let all the threads finish then combine the parsed data into the columnar data frame
     for t in threads {
         let mut x = t.join().unwrap();
         for i in 0..schema.len() {
